8dba404a8c189f92eb3fadbe53137b125a33c43f
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2022-12-03 10:47:43 +0100

    release: Festive Tech Calendar 2022

---
+++ Components/QRCodeBox.razor.js
@@ -0,0 +1,9 @@
+﻿export async function setQRCode(image, imageStream) {
+    const arrayBuffer = await imageStream.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    image.onload = () => {
+        URL.revokeObjectURL(url);
+    }
+    image.src = url;
+}
